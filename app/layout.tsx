a85c09f3461ae0f6da9a741e93f4200df2febe9e
Pranshu Dargar <pranshu11.pd@gmail.com>
2025-05-10 18:40:50 +0530

    Made changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-XXXXXXXXXX', {
+              gtag('config', 'G-1K6WFDL5B1', {
                 page_path: window.location.pathname,
               });
             `,
